677b9e6da398fe648a981e42eafc99c84904f39d
andynk1x <andyvarela090@gmail.com>
2026-04-23 16:54:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const grayBtn = document.querySelector('.gray-btn');
 const cartButton = document.getElementById('cartButton');
 const carImage = document.getElementById('carImage');
 
-// Al hacer clic en Rojo
+
 redBtn.addEventListener('click', () => {
     carImage.style.backgroundImage = "url('red-benz.png')";
     cartButton.style.backgroundColor = "red";
 });
 
-// Al hacer clic en Gris
+
 grayBtn.addEventListener('click', () => {
     carImage.style.backgroundImage = "url('gray-benz.png')";
     cartButton.style.backgroundColor = "#007bff";
